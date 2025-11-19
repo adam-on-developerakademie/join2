aff46ca7073cf0fdd82e92799106c234e07173d1
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-11-19 14:53:10 +0100

    feat: login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,22 @@
 import { Routes } from '@angular/router';
-import { crud } from  './crud/crud';
+import { crud } from './crud/crud';
 import { Contacts } from './contacts/contacts';
+import { Login } from './login/login';
+import { AppShell } from './shared/layout/app-shell/app-shell';
 
 export const routes: Routes = [
-     { path: '', component: Contacts },
-  { path: 'summary', redirectTo: '', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+{
+path: 'login', component: Login},
+
+{path: '', component: AppShell, children: [
+
+  { path: '', redirectTo: 'Contacts', pathMatch: 'full' },
+  { path: 'summary', redirectTo: 'contacts', pathMatch: 'full' },
   { path: 'contacts', component: Contacts },
   { path: 'board', component: crud },
   { path: 'crud', component: crud },
- // { path: 'addc', component: AddContactComponent },
-
-
+  // { path: 'addc', component: AddContactComponent },
+],
+},
 ];
