25b61ad85ad03332f9c05633045e8d5c1f9fc0a1
adamp <adam.piskorek@gmx.de>
2025-11-20 05:43:52 +0100

    Remove unused SVG icons and update login component paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { Tasktest } from './tasktest/tasktest';
 import { Contacts } from './contacts/contacts';
 import { Board } from './board/board';
 import { BoardCard } from './board/board-card/board-card';
+import { Login } from './login/login';
 
 export const routes: Routes = [
    { path: '', component: Board },
-   { path: 'summary', component: BoardCard },
+   { path: 'summary', component: Login },
    // { path: 'summary', redirectTo: '', pathMatch: 'full' },
    { path: 'contacts', component: Contacts },
    { path: 'board', component: Board },
